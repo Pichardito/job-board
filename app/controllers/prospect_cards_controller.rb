d292b44a66c0f46e4dde93eb9c8f17534ef86d47
mntj <mntj11@gmail.com>
2014-06-12 19:51:15 -0400

    add prospect_cards_controller, sessions_controller, prospect_cards index, root route

---
+++ app/controllers/prospect_cards_controller.rb
@@ -0,0 +1,31 @@
+class ProspectCardsController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def upate
+
+  end
+
+  def destroy
+
+  end
+
+end
