fd68d26c86d130ae3b4dd55ff719aabad407a3b9
mntj <mntj11@gmail.com>
2014-06-13 14:34:23 -0400

    change column name from isa to title. change routes for prospect_cards to resources format.

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140613161836) do
+ActiveRecord::Schema.define(version: 20140613174420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "prospect_cards", force: true do |t|
     t.string   "name"
-    t.string   "isa"
+    t.string   "title"
     t.string   "looking_for"
     t.text     "description"
     t.datetime "created_at"
