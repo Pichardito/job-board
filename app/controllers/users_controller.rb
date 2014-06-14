1b579707afe74b1ad2f90f3d60f9315a04d95e01
mntj <mntj11@gmail.com>
2014-06-14 19:30:24 -0400

    add missing routes for users. add user controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,37 @@
 class UsersController < ApplicationController
 
-def new
-  @user = User.new
-end
+  def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.create(user_params)
+    redirect_to login_path
+  end
 
-def create
-  @user = User.create(user_params)
-  redirect_to login_path
-end
+  def show
+    @user = User.find(params[:id])
+  end
 
-private
+  def edit
+    @user = User.find(params[:id])
+  end
 
-def user_params
-  params.require(:user).permit(:first_name, :last_name, :email, :password)
-end
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
+  end
+
+  def destroy
+    User.delete(params[:id])
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
 
 end
