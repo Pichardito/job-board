faa030c8ca6eebe4d3abe97e95863157cc0248b9
mntj <mntj11@gmail.com>
2014-06-14 15:42:31 -0400

    bugfix

--- app/controllers/prospect_cards_controller.rb
+++ app/controllers/prospect_cards_controller.rb
@@ -12,7 +12,7 @@ before_action :require_login
 
   def create
     prospect_card = ProspectCard.create(card_params)
-    current_user.prospect_card << prospect_card
+    current_user.prospect_cards << prospect_card
     redirect_to prospect_cards_path
   end
 
