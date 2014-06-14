8e52c87c4e5dd242069491b307980eb7b57e36d9
Pichardito <ctpichardo@gmail.com>
2014-06-14 12:45:02 -0400

    commit to overwrite welcome controller spec

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -14,12 +14,7 @@ describe WelcomeController do
         expect(actual).to eq(expected)
       end
 
-<<<<<<< HEAD
+
     end # GET index
 
 end #WelcomeController
-=======
-    end # GET INDEX
-
-end #WelcomeController
->>>>>>> cfd03295c1876f2e9279e0f91566040d539ee98f
