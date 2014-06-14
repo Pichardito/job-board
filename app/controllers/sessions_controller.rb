3369fc7d354ee1e4a60a1119cc908044c3cdf8d7
mntj <mntj11@gmail.com>
2014-06-13 21:25:26 -0400

    sessions controller prepull

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,9 +9,11 @@ class SessionsController < ApplicationController
     @user = User.find_by(:email => params[:email])
     if @user.id
       session[:user_id] = @user.id
+      binding.pry
     else
       user = User.from_omniauth(env["omniauth.auth"])
       session[:user_id] = user.id
+      binding.pry
     end
     redirect_to prospect_cards_path
   end
