dee1ceedfef483ae5b95d559b626259e7ff37b3a
mntj <mntj11@gmail.com>
2014-06-15 14:18:11 -0400

    remove binding.prys

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,7 @@ class UsersController < ApplicationController
 
   def update
     user = User.find(params[:id])
-    binding.pry
     user.update(user_params)
-    binding.pry
     redirect_to user_path(user)
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,6 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
-      binding.pry
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
