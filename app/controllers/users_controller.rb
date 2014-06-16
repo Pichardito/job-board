5adc61dc41180cb934e1238a2b617dd567a0251f
mntj <mntj11@gmail.com>
2014-06-16 16:08:35 -0400

    add back in ben's changes to user controller and methods for user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,26 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+       @user = User.find(params[:id])
     @prospect_card_likes = @user.prospect_card_likes.all
     @recruiter_card_likes = @user.recruiter_card_likes.all
+
+    #GETTING LIKES ASSOCIATED WITH USERS CARDS
+    #THESE GET ALL THE USERS PROSPECT AND RECRUITER CARDS
+    all_prospect_cards = @user.prospect_cards.all
+    all_recruiter_cards = @user.recruiter_cards.all
+
+    #THESE RETURN AN ARRAY OF THE CARD IDS BASED ON THE CARDS FOUND ABOVE
+    all_prospect_card_ids = @user.get_ids(all_prospect_cards)
+    all_recruiter_card_ids = @user.get_ids(all_recruiter_cards)
+
+    #THESE RETURN AN ARRAY OF THE LIKES ASSOCIATED WITH THE CARDS ABOVE
+    prospect_card_likes = @user.get_card_likes(all_prospect_card_ids, 'prospect')
+    recruiter_card_likes = @user.get_card_likes(all_recruiter_card_ids, 'recruiter')
+
+    #THIS RETURNS THE TOTAL NUMBER OF LIKES THE USER HAS RECEIVED
+    total_likes = prospect_card_likes.count + recruiter_card_likes.count
+
   end
 
   def edit
--- app/models/user.rb
+++ app/models/user.rb
@@ -35,4 +35,26 @@ validates :password, :presence => true,
     "Hi my name is #{self.name} and my email is #{self.email}"
   end
 
+   def get_ids(card_array)
+    id_array = []
+      card_array.each do |card|
+        id_array << card.id
+      end
+    return id_array
+  end
+
+  def get_card_likes(card_id_array, type)
+    liked_card_array = []
+    if type == 'prospect'
+      card_id_array.each do |card_id|
+        liked_card_array << ProspectCardLike.where(prospect_card_id: card_id)
+      end
+    else
+      card_id_array.each do |card_id|
+        liked_card_array << RecruiterCardLike.where(recruiter_card_id: card_id)
+      end
+    end
+      return liked_card_array
+  end
+
 end
