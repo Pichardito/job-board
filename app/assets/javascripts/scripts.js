26fa7d188e2b32a5be75e6520ac9217053f15a90
mntj <mntj11@gmail.com>
2014-06-16 16:14:29 -0400

    add carlos's updated scripts.js

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,4 +1,45 @@
 $(function(){
-  $('.prospect-card').draggable();
-  $('.recruiter-card').draggable();
+  $('.job-card').draggable();
+
+  $('.delete-pcard').on("click", function(e){
+
+    // $(this).parents('.prospect-card').remove()
+    e.preventDefault();
+
+    var that = this;
+    var cardId = $(this).parents('.prospect-card').data('id');
+      $.ajax({
+      url: '/prospect_cards/'+ cardId,
+        method: 'delete',
+        dataType: 'json',
+        data: {prospect_card: this.prospect_card},
+        success: function(data){
+        $(that).parents('.prospect-card').remove()
+        }
+    })
+
+    return false;
+
   })
+
+  $('.delete-rcard').on("click", function(e){
+
+    // $(this).parents('.recruiter-card').remove()
+    e.preventDefault();
+
+    var that = this;
+    var rcardId = $(this).parents('.recruiter-card').data('id');
+      $.ajax({
+      url: '/recruiter_cards/'+ rcardId,
+        method: 'delete',
+        dataType: 'json',
+        data: {recruiter_card: this.recruiter_card},
+        success: function(data){
+        $(that).parents('.recruiter-card').remove()
+        }
+    })
+
+    return false;
+
+  })
+})
