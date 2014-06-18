f403ecef97bb9b61235c8d9eb6a0bb1bbd840c85
mntj <mntj11@gmail.com>
2014-06-18 15:23:27 -0400

    route specs for prospect_cards

--- spec/routing/recruiter_cards_routes_spec.rb
+++ spec/routing/recruiter_cards_routes_spec.rb
@@ -1,1 +1,12 @@
 # RecruiterCard routes spec
+require 'spec_helper'
+
+describe 'routing for recruiter_cards' do
+  it 'routes / to welcome#index' do
+    expect(:get => '/').to route_to(
+      :action => 'index',
+      :controller => 'welcome'
+      )
+  end
+
+end
--- spec/routing/session_routes_spec.rb
+++ spec/routing/session_routes_spec.rb
@@ -4,22 +4,22 @@ require 'spec_helper'
 describe 'routing for sessions' do
   it 'routes /login to sessions#new' do
     expect(:get => '/login').to route_to(
-      :controller => 'sessions',
-      :action => 'new'
+      :action => 'new',
+      :controller => 'sessions'
       )
   end
 
   it 'routes /sessions to sessions#create' do
     expect(:post => '/sessions').to route_to(
-      :controller => 'sessions',
-      :action => 'create'
+      :action => 'create',
+      :controller => 'sessions'
       )
   end
 
   it 'routes /logout to sessions#destroy' do
     expect(:delete => '/logout').to route_to(
-      :controller => 'sessions',
-      :action => 'destroy'
+      :action => 'destroy',
+      :controller => 'sessions'
       )
   end
 end
