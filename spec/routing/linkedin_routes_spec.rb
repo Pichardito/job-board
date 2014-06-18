4e6aecf1f0920dfe5b552bb842192282da5659a1
mntj <mntj11@gmail.com>
2014-06-18 14:26:07 -0400

    add linkedin route specs

--- spec/routing/linkedin_routes_spec.rb
+++ spec/routing/linkedin_routes_spec.rb
@@ -1,1 +1,19 @@
 # LinkedIn routes spec
+require 'spec_helper'
+
+describe 'routing for linked_in' do
+  it 'routes /auth/:provider/callback to sessions#create' do
+    expect(:get => '/auth/:provider/callback').to route_to(
+      :controller => 'sessions',
+      :action => 'create',
+      :provider => ':provider'
+      )
+  end
+
+  it 'routes /auth/failure to welcome#index' do
+    expect(:get => '/auth/failure').to route_to(
+      :controller => 'welcome',
+      :action => 'index'
+      )
+  end
+end
