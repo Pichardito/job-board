6496a1ff0d60a0b8ea9c84cce8d990b3086689e4
mntj <mntj11@gmail.com>
2014-06-18 15:43:48 -0400

    add specs for recruiter_cards routes and add public/assets

--- spec/routing/recruiter_cards_routes_spec.rb
+++ spec/routing/recruiter_cards_routes_spec.rb
@@ -2,11 +2,71 @@
 require 'spec_helper'
 
 describe 'routing for recruiter_cards' do
-  it 'routes / to welcome#index' do
-    expect(:get => '/').to route_to(
+  it 'routes /recruiter_cards to recruiter_cards#index' do
+    expect(:get => '/recruiter_cards').to route_to(
       :action => 'index',
-      :controller => 'welcome'
+      :controller => 'recruiter_cards'
       )
   end
 
+  it 'routes /recruiter_cards/new to recruiter_cards#new' do
+    expect(:get => '/recruiter_cards/new').to route_to(
+      :action => 'new',
+      :controller => 'recruiter_cards'
+      )
+  end
+
+  it 'routes /recruiter_cards to recruiter_cards#create' do
+    expect(:post => '/recruiter_cards').to route_to(
+      :action => 'create',
+      :controller => 'recruiter_cards'
+      )
+  end
+
+  it 'routes /recruiter_cards/new to recruiter_cards#new' do
+    expect(:get => '/recruiter_cards/new').to route_to(
+      :action => 'new',
+      :controller => 'recruiter_cards'
+      )
+  end
+
+  it 'routes /recruiter_cards/:id/edit to recruiter_cards#edit' do
+    expect(:get => '/recruiter_cards/1/edit').to route_to(
+      :action => 'edit',
+      :controller => 'recruiter_cards',
+      :id => '1'
+      )
+  end
+
+  it 'routes /recruiter_cards/:id to recruiter_cards#show' do
+    expect(:get => '/recruiter_cards/1').to route_to(
+      :action => 'show',
+      :controller => 'recruiter_cards',
+      :id => '1'
+      )
+  end
+
+  it 'routes /recruiter_cards/:id to recruiter_cards#update using PUT' do
+    expect(:put => '/recruiter_cards/1').to route_to(
+      :action => 'update',
+      :controller => 'recruiter_cards',
+      :id => '1'
+      )
+  end
+
+  it 'routes /recruiter_cards/:id to recruiter_cards#update using PATCH' do
+    expect(:patch => '/recruiter_cards/1').to route_to(
+      :action => 'update',
+      :controller => 'recruiter_cards',
+      :id => '1'
+      )
+  end
+
+  it 'routes /recruiter_cards/:id to recruiter_cards#destroy' do
+    expect(:delete => '/recruiter_cards/1').to route_to(
+      :action => 'destroy',
+      :controller => 'recruiter_cards',
+      :id => '1'
+      )
+  end
 end
