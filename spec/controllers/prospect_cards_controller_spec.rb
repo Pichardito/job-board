678c3510748c6746eaa271067ee65098594b4b70
mntj <mntj11@gmail.com>
2014-06-18 12:32:52 -0400

    recruiter and prospect cards controller specs now match up nicely

--- spec/controllers/prospect_cards_controller_spec.rb
+++ spec/controllers/prospect_cards_controller_spec.rb
@@ -24,30 +24,99 @@ describe ProspectCardsController do
         @prospect_card_test.save!
       end
 
+      # GET show
+      describe 'GET show' do
+        before :each do
+          get :show, :id => @prospect_card_test.id
+        end
 
-        # --- INDEX ---
-    describe 'GET show' do
-
-      before :each do
-        get :index
-      end
-
-      it 'responds successfully' do
-        actual = response.code
-        expected = '200'
-        expect(actual).to eq(expected)
-      end
-
-      it 'assigns @prospect_card' do
-          actual = assigns(:prospect_card_test)
-          expected = @prospect_card
+        it 'responds successfully' do
+          actual = response.code
+          expected = '200'
           expect(actual).to eq(expected)
         end
 
-    end # GET show
+        it 'assigns @prospect_card' do
+            actual = assigns(:prospect_card_test)
+            expected = @prospect_card
+            expect(actual).to eq(expected)
+        end
 
-  # TESTS CREATION OF NEW CARD
-   # --- NEW ---
+        it 'renders the show template' do
+          expect(response).to render_template('show')
+        end
+      end # GET show
+
+      # GET edit
+      describe 'GET edit' do
+        before :each do
+            get :edit, :id => @prospect_card_test.id
+          end
+
+        it 'responds successfully' do
+          actual = response.code
+          expected = '200'
+          expect(actual).to eq(expected)
+        end
+
+        it 'assigns @prospect_card' do
+            actual = assigns(:prospect_card_test)
+            expected = @prospect_card
+          end
+
+        it 'renders the edit template' do
+          expect(response).to render_template('edit')
+        end
+      end # GET edit
+
+      # POST update
+      describe "POST update" do
+        before :each do
+              post :update, {:id => @prospect_card_test.id, :prospect_card => { name: 'Larry Page', title: 'Entrepreneur', looking_for: 'computer scientists', description: 'come work for Google!', left_pos: 100, top_pos: 100 }}
+        end
+
+        it "updates the ProspectCard record" do
+          @prospect_card_test.reload
+
+          actual = @prospect_card_test.name
+          expected = 'Larry Page'
+          expect(actual).to eq(expected)
+
+          actual = @prospect_card_test.title
+          expected = 'Entrepreneur'
+          expect(actual).to eq(expected)
+
+          actual = @prospect_card_test.looking_for
+          expected = 'computer scientists'
+          expect(actual).to eq(expected)
+
+          actual = @prospect_card_test.description
+          expected = 'come work for Google!'
+          expect(actual).to eq(expected)
+
+          actual = @prospect_card_test.left_pos
+          expected = 100
+          expect(actual).to eq(expected)
+
+          actual = @prospect_card_test.top_pos
+          expect(actual).to eq(expected)
+        end
+
+        it "responds with JSON" do
+          response.header['Content-Type'].should include 'application/json'
+        end
+      end # POST update
+
+      # DELETE destroy
+      describe "DELETE destroy" do
+        it 'destroys a prospect_card record' do
+          expect { delete :destroy, {:id => @prospect_card_test.id }}
+           .to change(ProspectCard, :count).by(-1)
+        end
+      end # DELETE destroy
+    end # Given a prospect card
+
+  # GET new
   describe 'GET new' do
     before :each do
       get :new
@@ -69,85 +138,18 @@ describe ProspectCardsController do
     end
   end # GET new
 
-  # --- CREATE ---
-
-
-    # --- EDIT ---
-    describe 'GET edit' do
-      before :each do
-          get :edit, :id => @prospect_card_test.id
-        end
-
-      it 'responds successfully' do
-        actual = response.code
-        expected = '200'
-        expect(actual).to eq(expected)
-      end
-
-      it 'assigns @prospect_card' do
-          actual = assigns(:prospect_card_test)
-          expected = @prospect_card
-        end
-
-      it 'renders the edit template' do
-        expect(response).to render_template('edit')
-      end
-
-    end # GET edit
-
-
-    describe "POST update" do
-      before :each do
-            post :update, {:id => @prospect_card_test.id, :prospect_card => { name: 'Larry Page', title: 'Entrepreneur', looking_for: 'computer scientists', description: 'come work for Google!', left_pos: 100, top_pos: 100 }}
-      end
-
-      it "updates the ProspectCard record" do
-        @prospect_card_test.reload
-
-        actual = @prospect_card_test.name
-        expected = 'Larry Page'
-        expect(actual).to eq(expected)
-
-        actual = @prospect_card_test.title
-        expected = 'Entrepreneur'
-        expect(actual).to eq(expected)
-
-        actual = @prospect_card_test.looking_for
-        expected = 'computer scientists'
-        expect(actual).to eq(expected)
-
-        actual = @prospect_card_test.description
-        expected = 'come work for Google!'
-        expect(actual).to eq(expected)
-
-        actual = @prospect_card_test.left_pos
-        expected = 100
-        expect(actual).to eq(expected)
-
-        actual = @prospect_card_test.top_pos
-        expect(actual).to eq(expected)
-      end
-
-      it "responds with JSON" do
-        response.header['Content-Type'].should include 'application/json'
-      end
-    end # POST update
-
-
-    describe "DELETE destroy" do
-
-      it "deletes prospect_card" do
-        expect { delete :destroy, {:id => @prospect_card_test.id }}
-         .to change(ProspectCard, :count).by(-1)
-      end
-
-    end # DELETE destroy
-  end # Given a prospect card
-
+    # POST create
     describe 'POST create' do
-
       before :each do
         post :create, {:prospect_card => { name: 'Carlos', title: 'manager', looking_for: 'employee', description: 'this' }}
+      end
+
+      it 'creates a ProspectCard with the correct attribute values' do
+        new_prospect_card = ProspectCard.find_by(name: 'Carlos')
+        expect(new_prospect_card.name).to eq('Carlos')
+        expect(new_prospect_card.title).to eq('manager')
+        expect(new_prospect_card.looking_for).to eq('employee')
+        expect(new_prospect_card.description).to eq('this')
       end
 
       it 'responds with a redirect' do
