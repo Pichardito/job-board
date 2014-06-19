793de629ca6e6d6a1991b350d5e0b1ef061cfc6f
mntj <mntj11@gmail.com>
2014-06-18 20:51:17 -0400

    merge conflict with scripts.js

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -42,23 +42,6 @@ $(function(){
 
   //********PREVENT TEXT OVERFLOW ON CARD TEXT FIELDS**********//
 
-  // $('.card-header').each(function(idx, headerEl){
-  //   debugger;
-  //   while ( $(headerEl).width() > 250 ) {
-  //     var fontSize = parseInt($(headerEl).css('font-size'));
-  //     fontSize = fontSize - 5;
-  //     $(headerEl).css('font-size', fontSize);debugger;
-  //   };
-  // });
-
-  // var cardTexts = $('.card-text card-description', '.card-text card-looking_for', '.card-text card-title');
-  // $.each(cardTexts, function(idx, textEl){
-  //   while ( $(textEl).height() > 100 ) {
-  //     var fontSize = parseInt($(textEl).css('font-size'));
-  //     fontSize = fontSize - 5;
-  //     $(textEl).css('font-size', fontSize);
-  //   };
-  // });
 
   var cardTexts = $('.card-text');
   $.each(cardTexts, function(idx, textEl){
@@ -141,52 +124,6 @@ $('.like-rcard').on("click", function(e){
 
 //********* EDIT CARDS WITH AJAX FUNCTIONS**********//
 
-// $(".card-text").on('hover', function(ele){
-//     var cardId = parseInt(this.parentElement.dataset.id);
-//     $(this).editInPlace({
-//         callback: function(unused, enteredText){
-//           $.ajax({
-//                 url: '/prospect_cards/'+ cardId,
-//                 method: 'put',
-//                 dataType: 'json',
-//                 data: {prospect_card: {
-//                 title: $(this).parent().find('.card-title').html(),
-//                 description: $(this).parent().find('.card-description').html(),
-//                 looking_for: $(this).parent().find('.card-looking_for').html()
-//                 }},
-//                 success: function(data){
-//                   console.log(data)
-//                 }
-//           })
-//           return enteredText;
-//         }
-//     })
-// });
-
-<<<<<<< HEAD
-// $(".rcard-text").each(function(ele){
-//     var cardId = parseInt(this.parentElement.dataset.id);
-//     $(this).editInPlace({
-//         callback: function(unused, enteredText){
-//           $.ajax({
-//                 url: '/recruiter_cards/'+ cardId,
-//                 method: 'put',
-//                 dataType: 'json',
-//                 data: { recruiter_card: {
-//                   title: $(this).parent().find('.card-title').html(),
-//                   description: $(this).parent().find('.card-description').html(),
-//                   looking_for: $(this).parent().find('.card-looking_for').html()
-//                   }
-//                 },
-//                 success: function(data){
-//                   console.log(data)
-//                 }
-//           })
-//           return enteredText;
-//         }
-//     });
-// });
-=======
 $(".card-text").each(function(ele){
     var cardId = parseInt(this.parentElement.dataset.id);
     var cardUserID = parseInt(this.parentElement.dataset.userId);
@@ -266,6 +203,5 @@ $(".rcard-text").each(function(ele){
       $(textEl).css('font-size', fontSize);
     };
   });
->>>>>>> 64c1a522188610cc2dddc0e54213a0101476ca4e
 
 });
